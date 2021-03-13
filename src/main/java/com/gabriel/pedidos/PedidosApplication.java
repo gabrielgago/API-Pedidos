caf302a95fd21b6ab3875f7931cffd637969a0ef
Gabriel Frael <gfrael@gmail.com>
2021-03-13 14:26:04 -0300

    Adicionado relacionamento cidade/estado

--- src/main/java/com/gabriel/pedidos/PedidosApplication.java
+++ src/main/java/com/gabriel/pedidos/PedidosApplication.java
@@ -9,42 +9,63 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.gabriel.pedidos.domain.Categoria;
+import com.gabriel.pedidos.domain.Cidade;
+import com.gabriel.pedidos.domain.Estado;
 import com.gabriel.pedidos.domain.Produto;
 import com.gabriel.pedidos.repositories.CategoriaRepository;
+import com.gabriel.pedidos.repositories.CidadeRepository;
+import com.gabriel.pedidos.repositories.EstadoRepository;
 import com.gabriel.pedidos.repositories.ProdutoRepository;
 
 @SpringBootApplication
-public class PedidosApplication implements CommandLineRunner{
+public class PedidosApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(PedidosApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
+
 		Produto p1 = new Produto(null, "Computador", BigDecimal.valueOf(2000.00));
 		Produto p2 = new Produto(null, "Impressora", BigDecimal.valueOf(800.00));
 		Produto p3 = new Produto(null, "Mouse", BigDecimal.valueOf(80.00));
-		
-		cat1.getProdutos().addAll(List.of(p1,p2,p3));
+
+		cat1.getProdutos().addAll(List.of(p1, p2, p3));
 		cat2.getProdutos().addAll(List.of(p2));
-		categoriaRepository.saveAll(List.of(cat1, cat2));
-		
+
 		p1.getCategorias().addAll(List.of(cat1));
 		p2.getCategorias().addAll(List.of(cat1, cat2));
 		p3.getCategorias().addAll(List.of(cat1));
-		
-		produtoRepository.saveAll(List.of(p1,p2,p3));
-		
+
+		categoriaRepository.saveAll(List.of(cat1, cat2));
+		produtoRepository.saveAll(List.of(p1, p2, p3));
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().addAll(List.of(c1));
+		est2.getCidades().addAll(List.of(c2, c3));
+
+		estadoRepository.saveAll(List.of(est1, est2));
+		cidadeRepository.saveAll(List.of(c1, c2, c3));
+
 	}
 
 }
